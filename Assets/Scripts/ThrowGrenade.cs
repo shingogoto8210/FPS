022e6a06c49042909a4abd2722dd9b8f3d618933
shingogoto8210 <chupps8210@gmail.com>
2021-06-20 12:53:08 +0900

    12　手榴弾を自爆させる　完了

---
+++ Assets/Scripts/ThrowGrenade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrenade : MonoBehaviour
+{
+    public GameObject grenadePrefab;
+    public AudioClip throwSound;
+    public float throwSpeed;
+    public int grenadeCount;
+    private float timeBetweenThrow = 2f;
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Mouse0) && grenadeCount > 0 && timer > timeBetweenThrow)
+        {
+            grenadeCount -= 1;
+            timer = 0;
+
+            GameObject grenade = Instantiate(grenadePrefab, transform.position, Quaternion.identity);
+            Rigidbody grenadeRb = grenade.GetComponent<Rigidbody>();
+            grenadeRb.AddForce(transform.forward * throwSpeed);
+            AudioSource.PlayClipAtPoint(throwSound, Camera.main.transform.position);
+        }
+    }
+}
