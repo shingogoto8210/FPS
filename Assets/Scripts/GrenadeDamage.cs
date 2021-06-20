290c2800ad07cc6dd535ef1685ba77f07ca2a47c
shingogoto8210 <chupps8210@gmail.com>
2021-06-20 14:59:00 +0900

    14　手榴弾で壁を壊す　完了

---
+++ Assets/Scripts/GrenadeDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeDamage : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
