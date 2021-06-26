dd4d82007d94311768e6c4dc86a51e24084f0953
shingogoto8210 <chupps8210@gmail.com>
2021-06-26 11:20:46 +0900

    敵が一定間隔で砲弾を撃つ

---
+++ Assets/Scripts/EnemyTurret.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public GameObject enemyShellPrefab;
+    private int interval;
+    public int shotPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interval += 1;
+        if(interval % 60 == 0)
+        {
+            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
+            Rigidbody rb = enemyShell.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * shotPower);
+            Destroy(enemyShell, 2.0f);
+        }
+    }
+}
