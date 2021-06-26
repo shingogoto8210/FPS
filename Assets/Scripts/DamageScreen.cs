0e7295773f7484a71d7d5388aad80b2c42b92e89
shingogoto8210 <chupps8210@gmail.com>
2021-06-26 12:36:50 +0900

    敵の攻撃で画面が赤くなる

---
+++ Assets/Scripts/DamageScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageScreen : MonoBehaviour
+{
+    public Image damageScreenImage;
+    private Color color;
+    public int playerHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageScreenImage = GameObject.Find("DamageScreen").GetComponent<Image>();
+        color = damageScreenImage.color;
+        color.a = 0.0f;
+        damageScreenImage.color = color;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "EnemyShell")
+        {
+            playerHP -= 1;
+            Destroy(other.gameObject);
+            color.a += 0.1f;
+            damageScreenImage.color = color;
+        }
+
+    }
+
+}
